8b6fefb9785b1194077b56f6a3dcb66339ef0663
Solokudon <itetteh18@icloud.com>
2022-12-08 04:15:36 -0800

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry
- *
- * Return: Always ) (Success)
- */
+* main - Entry
+*
+* Return: Always ) (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 
          	return (0);
-
 }	
