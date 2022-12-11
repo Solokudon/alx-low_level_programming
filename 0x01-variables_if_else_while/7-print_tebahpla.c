61cca03f368de7e217cbcf36fef635b6b95da64e
Solokudon <itetteh18@icloud.com>
2022-12-11 05:37:43 -0800

    Codefun

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -lowercase alpabets in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'z'; la >= 'a'; la--)
+
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+
+}
