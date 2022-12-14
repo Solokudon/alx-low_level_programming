42a49a20671d10645cee3ff11fc44d06ed9247d0
Solokudon <itetteh18@icloud.com>
2022-12-14 06:49:48 -0800

    task5fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,8 @@
  * print_sign - prints the sign of a number
  * @n: the number of the sign to be printed
  * Return: 1 if number is greater than zero
- * 0 if number is less than zero
+ * 0 and print 0 if number is zero
+ * -1 and prints - if n is less than zero
  */
 
 int print_sign(int n)
@@ -15,7 +16,7 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		-putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else
